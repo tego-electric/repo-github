ddb97f7fdda9fcac329683672b64b90a70a2a477
Eduardo <silvaeduardo1292@gmail.com>
2020-08-11 17:14:40 -0300

    cambio

--- js/products.js
+++ js/products.js
@@ -1,6 +1,19 @@
+var arrayproductos = [];
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(LISTA_ACTORES).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            arrayproductos = resultObj.data;
+            //Muestro las categorías ordenadas
+            crearElementosBasicos();
+            productos(arrayproductos);
+        }
+    });
 });
+
+
+function productos(array){
+
+}
