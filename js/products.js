0b1e9fb5853d49db32f06f30e74bfc0b65e6775d
Eduardo <silvaeduardo1292@gmail.com>
2020-08-14 17:43:34 -0300

    desafiate

--- js/products.js
+++ js/products.js
@@ -19,7 +19,7 @@ function productosDetalle(array){
     var container_div = document.getElementById("containerDecks");
     
     for (i = 0; i<array.length;i++){
-        if  (i%3 == 0){
+        /*if  (i%3 == 0){
                 if (i!= 0){
                     container_div.lastChild.innerHTML = htmlAppendToInner;
                     htmlAppendToInner = "";
@@ -27,22 +27,25 @@ function productosDetalle(array){
                 var newrow = document.createElement("div");
                 newrow.className = "row";
                 container_div.appendChild(newrow);
-        }
+        }*/
         let dato = array[i];
         htmlAppendToInner+= `
-        <div class="col-sm-4" style ="padding: 10px ">
-        <div class="card"">
-            <div class="card-header" style="text-align: right">Sold count: `+ dato.soldCount +`</div>
-            <img src="`+ dato.imgSrc + `" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">`+ dato.name +`</h5>
-                <p class="card-text">`+ dato.description +`</p>
-            </div>
-            <div class="card-footer">
-                <div style="font-size:25px; vertical-align:middle">`+ dato.currency+ `: `+dato.cost+`</div>
+        <div class="col-sm-4" style ="padding: 10px;">
+            <div class="card"">
+                <div class="card-header" style="text-align: right">Sold count: `+ dato.soldCount +`</div>
+                <img src="`+ dato.imgSrc + `" class="card-img-top" alt="`+ dato.name +`">
+                <div class="card-body">
+                    <h5 class="card-title">`+ dato.name +`</h5>
+                    <p class="card-text">`+ dato.description +`</p>
+                </div>
+                <div class="card-footer">
+                    <div style="font-size:25px; vertical-align:middle">`+ dato.currency+ `: `+dato.cost+`</div>
+                </div>
             </div>
         </div>
-        </div>`
+        `
+        container_div.innerHTML= htmlAppendToInner;
     }
-    document.getElementById("containerDecks").lastChild.innerHTML = htmlAppendToInner;
+    //container_div.lastChild.innerHTML = htmlAppendToInner;
+    
 }
