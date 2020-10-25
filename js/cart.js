7d431d3c0ff0e108f2e58a899aa6253fa99f6031
Eduardo <silvaeduardo1292@gmail.com>
2020-10-25 10:46:48 -0300

    entrega 6

--- js/cart.js
+++ js/cart.js
@@ -2,6 +2,7 @@ const MONEDA_PESOS = "pesos"
 const MONEDA_DOLARES = "dolares"
 var listaArticulos = [];
 var moneda = undefined;
+var mensaje = undefined;
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -13,12 +14,35 @@ document.addEventListener("DOMContentLoaded", function(e){
             showCartProduct(MONEDA_PESOS,listaArticulos);
         }
     });
+    getJSONData(CART_BUY_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            mensaje = (resultObj.data).msg;
+        }
+    });
     document.getElementById("monedaPesos").addEventListener("click", function(){
         showCartProduct(MONEDA_PESOS, listaArticulos);
     });
     document.getElementById("monedaDolares").addEventListener("click", function(){
         showCartProduct(MONEDA_DOLARES, listaArticulos);
     });
+
+    // listeners que validan que solo se ingresen valores de cierto tipo
+    document.getElementById("puerta").addEventListener("keypress",function(e){
+        if (!soloNumeros(e)){
+            e.preventDefault();
+        }  
+    })
+    document.getElementById("direccion").addEventListener("keypress",function(e){
+        if (!soloLetrasNumeros(e)){
+            e.preventDefault();
+        }  
+    })
+    document.getElementById("esquina").addEventListener("keypress",function(e){
+        if (!soloLetrasNumeros(e)){
+            e.preventDefault();
+        }  
+    })
+    
 });
 
 // se muestra el carrito de compra en pantalla
@@ -55,7 +79,7 @@ function showCartProduct(tipoMoneda, array){
                     <div class="row justify-content-between">                  
                         <div class="col">
                             <label>Cantidad</label>
-                            <input onchange="precioTotal()" name="cantidad" type="number" value="`+ product.count + `" min ="1" max="50" step="1"></input>
+                            <input onchange="precioSubtotal()" name="cantidad" type="number" value="`+ product.count + `" min ="1" max="50" step="1"></input>
                         </div>
                         <span class="text-muted">Precio unitario: ` + monedaProducto + " "+`<span name="costounit">  `+ precioProducto + `</span></span>
                     </div>
@@ -67,11 +91,11 @@ function showCartProduct(tipoMoneda, array){
         `
     }
     document.getElementById("contenedor-lista").innerHTML = htmlcontentToAppend;
-    precioTotal();
+    precioSubtotal();
 }
 
 // calcula los precios por cantidades y los suma al subtotal
-function precioTotal(){
+function precioSubtotal(){
     let cantidad = document.getElementsByName("cantidad");
     let costo = document.getElementsByName("costounit");
     let precioPrevio = document.getElementsByName("precio-cantidad");
@@ -109,6 +133,15 @@ function listaProdAMetodoEnvio(){
         link2.setAttribute("class", "nav-link active");
         panel1.setAttribute("class", "tab-pane fade");
         panel2.setAttribute("class", "tab-pane fade show active");
+
+        var porcentaje = document.getElementsByName("radiobuttonenvio");
+        var val = undefined;
+        for (let i = 0; i< porcentaje.length ; i++){
+            if (porcentaje[i].checked){
+                val = porcentaje[i].value;
+            }
+        }
+        precioTotal(val);
     }else{document.getElementById("error-subtotal").style.display = "block";}
 }
 
@@ -140,4 +173,333 @@ function eliminarArticulo(idproducto){
     listaArticulos = nuevoArray;
     idproducto.remove();
     showCartProduct(moneda, listaArticulos);
+}
+
+function precioTotal(porcentaje){
+    var valor = parseInt(document.getElementById("subtotal").innerHTML);
+    var valortotal = 0;
+    if (porcentaje != undefined){
+        valortotal = (parseInt(porcentaje)*valor)/100;
+        valortotal += valor;
+        var tipoCambio = undefined;
+        if (moneda == MONEDA_DOLARES){
+            tipoCambio = "USD";
+        }else{tipoCambio = "$";}
+        document.getElementById("subtotalModal").innerHTML = " " + tipoCambio +  valor;
+        document.getElementById("envioModal").innerHTML = " " + tipoCambio + (valortotal - valor);
+        document.getElementById("totalModal").innerHTML = " " + tipoCambio +  valortotal;
+    }
+    document.getElementById("total").innerHTML = valortotal;
+}
+
+//
+// funcion que muestra el conteiner del metodo de pago
+//
+
+function mostarContainer(valor){
+    var htmlToAppend = "";
+    if (valor == "credito"){
+        htmlToAppend = `
+        <div class="col-md-12">
+            <div class="row pb-2">
+                <div class="col"><b><span style="text-decoration: underline">Tarjeta de credito</span></b></div>
+            </div>
+            <div class="row">
+                <div class="col-md-5"><label for="digitosTarjeta">Digitos de la tarjeta: </label></div>
+                <div class="col"><input type="text" id="digitosTarjeta" maxlength="20" onblur="validarTarjeta(this.value)"></div>
+                <label class ="container label-error" id="error-digitosCard">Digitos incorrectos</label>
+            </div>
+            <div class="row">
+                <div class="col-md-5"><label for="fechaExpiracion">Fecha de expiracion: </label></div>
+                <div class="col"><input type="date" id="fechaExpiracion" onblur="validarFecha(this.value)"></div>
+                <label class ="container label-error" id="error-fechaExpiracion">Fecha mal ingresada</label>
+            </div>
+            <div class="row">
+                <div class="col-md-5"><label for="numVerificacion">Digitos de verificacion: </label></div>
+                <div class="col"><input type="text" id="numVerificacion" maxlength="3" onblur="validarDigitos(this.value)"></div>
+                <label class ="container label-error" id="error-numVerificacion">Digitos incorrectos</label>
+            </div>
+        </div>
+        `;        
+    }    else{
+        var dir = document.getElementById("direccion").value;
+        var monto = document.getElementById("total").innerHTML;
+        htmlToAppend = `
+        <div class="col-md-12">
+            <div class="row pb-2">
+                <div class="col"><b><span style="text-decoration: underline">Transferencia bancaria</span></b></div>
+            </div>
+            <div class="row">
+                <div class="col-md-1"><label for="paisPAgoBancario">Pais:</label></div>
+                <div class="col-md-4"><input type="text" id="paisPagoBancario" onblur="validarPaisTransferencia(this.value)"></div>
+                <label class ="container label-error" id="error-paisPagoBancario">Pais no valido</label>
+            </div>
+            <div class="row">
+                <div class="col-md-8"><label for="direccionMetodo">Direccion:`+ `  ` + dir+`</label></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"><label for="numCuetna">Num. de cuenta: </label></div>
+                <div class="col-md-3"><input type="text" id="numCuenta" maxlength="10" onblur="validarCuenta(this.value)"></div>
+                <label class ="container label-error" id="error-numCuenta">Digitos incorrectos</label>
+            </div>
+            <div class="row">
+                <div class="col-md-8"><label for="">Codigo SWIFT/BIC: AAAA-BB-CC-123</label></div>
+            </div>
+            <div class="row">
+                <div class="col-md-8"><label for="">Divisa: `+moneda+` </label></div>
+            </div>
+            <div class="row">
+                <div class="col-md-8"><label for="">Monto: `+monto+` </label></div>
+            </div>
+        </div>
+        `;        
+    }
+    var container = document.getElementById("containerMetodoPAgo");
+    container.innerHTML = htmlToAppend;
+
+    // listener para validar el ingreso de valores del tipo correspondiente
+    if (valor == "credito"){
+        document.getElementById("digitosTarjeta").addEventListener("keypress",function(e){
+            if (!soloNumeros(e)){
+                e.preventDefault();
+            }  
+        })
+        document.getElementById("numVerificacion").addEventListener("keypress",function(e){
+            if (!soloNumeros(e)){
+                e.preventDefault();
+            }  
+        })
+    }else{
+        document.getElementById("numCuenta").addEventListener("keypress",function(e){
+            if (!soloNumeros(e)){
+                e.preventDefault();
+            }  
+        }) 
+        document.getElementById("paisPagoBancario").addEventListener("keypress",function(e){
+            if (!soloLetras(e)){
+                e.preventDefault();
+            }  
+        })
+    }
+}
+
+
+// 
+// sector de validaciones metodo de envio
+// 
+
+
+// validacion direccion
+function validarDireccion(){
+    var dir = document.getElementById("direccion").value;
+    if (dir == ""){
+        document.getElementById("error-direccion").style.display = "block";
+        return false;       
+    }else{
+        document.getElementById("error-direccion").style.display = "none";
+        return true;
+    }
+}
+
+// validacion puerta
+function validarPuerta(){
+    var puerta = document.getElementById("puerta").value;
+    if (puerta == ""){
+        document.getElementById("error-puerta").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("error-puerta").style.display = "none";
+        return true;
+    }
+}
+
+// validacion esquina
+function validarEsquina(){
+    var esquina = document.getElementById("esquina").value;
+    if (esquina == ""){
+        document.getElementById("error-esquina").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("error-esquina").style.display = "none";
+        return true;
+    }
+}
+
+// validacion paises
+function ValidarPais(val){
+    if (val == "nada"){
+        document.getElementById("error-pais").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("error-pais").style.display = "none";
+        return true;
+    }
+}
+
+// validacion radios
+function validarRadioEnvio(){
+    var arrayRadios = document.getElementsByName("radiobuttonenvio");
+    var noestanMarcados = false;
+    for (let i = 0; i< arrayRadios.length ; i++){
+        if (arrayRadios[i].checked){
+            noestanMarcados = true;
+        }
+    }
+    if (noestanMarcados){
+        document.getElementById("error-selector").style.display = "none";
+        return true;
+    }else{
+        document.getElementById("error-selector").style.display = "block";
+        return false;
+    }
+}
+
+
+function validarMetodoDeEnvio(){
+
+    var dir = validarDireccion();
+    var esquina = validarEsquina();
+    var puerta = validarPuerta();
+    var radio = validarRadioEnvio();
+    var pais = ValidarPais(document.getElementById("pais").value);
+    var todook = dir && esquina && puerta && radio && pais; 
+
+    if (todook){
+        $('#modalPago').modal('show')
+    }
+}
+
+
+// 
+// validacion de metodo de pago
+// 
+
+
+// valida que haya seleccionado algun metodo de pago
+function validarRadioPago(){
+    var arrayRadios = document.getElementsByName("radiobuttonpago");
+    var noestanMarcados = false;
+    for (let i = 0; i< arrayRadios.length ; i++){
+        if (arrayRadios[i].checked){
+            noestanMarcados = true;
+        }
+    }
+    if (noestanMarcados){
+        document.getElementById("error-metodopago").style.display = "none";
+        return true;
+    }else{
+        document.getElementById("error-metodopago").style.display = "block";
+        return false;
+    }
+}
+
+// validar campos de transferencia bancaria
+function validarCuenta(cuenta){
+    if (cuenta.length != 10){
+        document.getElementById("error-numCuenta").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("error-numCuenta").style.display = "none";
+        return true;
+    }
+}
+
+
+function validarPaisTransferencia(pais){
+    if (pais.length == 0){
+        document.getElementById("error-paisPagoBancario").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("error-paisPagoBancario").style.display = "none";
+        return true;
+    }
+}
+
+
+
+// validar campos de credito
+function validarDigitos(digitos){
+    if (digitos.length != 3){
+        document.getElementById("error-numVerificacion").style.display = "block";
+        return false;
+    }else {
+        document.getElementById("error-numVerificacion").style.display = "none";
+        return true;
+    }
+}
+
+function validarFecha(fecha){
+    if (fecha == ""){
+        document.getElementById("error-fechaExpiracion").style.display = "block";
+        return false;
+    }else {
+        document.getElementById("error-fechaExpiracion").style.display = "none";
+        return true;
+    }
+}
+
+function validarTarjeta(tarjeta){
+    if (tarjeta.length != 20){
+        document.getElementById("error-digitosCard").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("error-digitosCard").style.display = "none";
+        return true;
+    }
+}
+
+// validar que todos los campos esten completos y bien rellendos
+
+function validarCompra(){
+    var validoRadio = validarRadioPago();
+    var arrayRadios = document.getElementsByName("radiobuttonpago");
+    var tipoPago = "";
+    for (let i = 0; i< arrayRadios.length ; i++){
+        if (arrayRadios[i].checked){
+            tipoPago = arrayRadios[i].value;
+        }
+    }
+    if (tipoPago == "credito"){
+        var digitos = validarTarjeta(document.getElementById("digitosTarjeta").value);
+        var fecha = validarFecha(document.getElementById("fechaExpiracion").value);
+        var numero = validarDigitos(document.getElementById("numVerificacion").value);
+        if (validoRadio && digitos && fecha && numero){
+            $('#modalPago').modal('hide');            
+            $('#modalMensaje').modal('show');
+            document.getElementById("mensajeSatisfactorio").innerHTML = mensaje;
+        }
+    }else{
+        var pais = validarPaisTransferencia(document.getElementById("paisPagoBancario").value);
+        var cuenta = validarCuenta(document.getElementById("numCuenta").value);
+        if (validoRadio && pais && cuenta){
+            $('#modalPago').modal('hide');
+            $('#modalMensaje').modal('show');
+            document.getElementById("mensajeSatisfactorio").innerHTML = mensaje;
+        }
+    }
+}
+
+
+// 
+// validar que son solo numeros
+// 
+function soloNumeros(val){
+    var key = val.keyCode;
+    return key >= 48 && key <= 57;
+}
+
+// 
+// valdiar que solo sean letras
+// 
+function soloLetras(val){
+    var key = val.keyCode;
+    return (key >= 65 && key <= 90) || (key >= 97 && key <= 122);
+}
+
+// 
+// Validar que solo sean numeros o letras
+// 
+function soloLetrasNumeros(val){
+    var key = val.keyCode;
+    return (key >= 65 && key <= 90) || (key >= 97 && key <= 122) || (key >= 48 && key <= 57);
 }
